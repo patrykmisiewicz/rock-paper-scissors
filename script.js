cc10f4305497604d3050f97d82ab3b573fe3b9c6
patrykmisiewicz <patrykmisiewicz1@gmail.com>
2023-04-14 20:07:48 +0200

    Make function for random choice

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("Hello");
+
+
+function getComputerChoice() {
+
+    let number = Math.random()
+
+    console.log(number);
+
+        if (number > 0.66) {
+            console.log("Rock");
+        } else if (number < 0.33) {
+            console.log("Paper");
+        } else {
+            console.log("Scissors")
+        }
+    }
+
+getComputerChoice()
