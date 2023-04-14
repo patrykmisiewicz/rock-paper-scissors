6d8e8fdd4f67ebcc4f4ffeb23f1ca36c1955b4c7
patrykmisiewicz <patrykmisiewicz1@gmail.com>
2023-04-14 20:53:03 +0200

    Create function for random choice, and function to play a round

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
-console.log("Hello");
-
-
 function getComputerChoice() {
 
     let number = Math.random()
 
-    console.log(number);
-
         if (number > 0.66) {
-            console.log("Rock");
+            return "rock";
         } else if (number < 0.33) {
-            console.log("Paper");
+            return "paper";
         } else {
-            console.log("Scissors")
+            return "scissors";
         }
+        
     }
 
-getComputerChoice()
+
+let playerSelection = prompt("What do you choose?", '') 
+
+playerSelection = playerSelection.toLowerCase();
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "Draw!";
+    } else if (playerSelection === "rock" && computerSelection === "paper" ||
+    playerSelection === "scissors" && computerSelection === "rock" ||
+    playerSelection === "paper" && computerSelection === "scissors") {
+        return `You lose, ${computerSelection} beats ${playerSelection}!`
+    } else if (playerSelection === "paper" && computerSelection === "rock" ||
+    playerSelection === "rock" && computerSelection === "scissors" ||
+    playerSelection === "scissors" && computerSelection === "paper") {
+        return `You won, ${playerSelection} beats ${computerSelection}!`
+    } else {
+        return "You can only choose rock, scissors or paper!";
+    }
+}
+
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
